01b06d4328e7ab53dc2d161457d7bb76223d518e
IvanenkovSergey <ivanenkovserg@gmail.com>
2022-08-17 16:37:57 +0300

    first

---
+++ src/main/java/guru/qa/gitConflict.java
@@ -0,0 +1,9 @@
+package guru.qa;
+
+public class gitConflict {
+    public static void main(String[] args) {
+        String conflictName = "First";
+
+        System.out.println(conflictName);
+    }
+}
