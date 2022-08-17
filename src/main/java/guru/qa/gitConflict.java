9a99eb447c7d2706da9e24322212976392de654e
IvanenkovSergey <ivanenkovserg@gmail.com>
2022-08-17 16:54:25 +0300

    Commit2

--- src/main/java/guru/qa/gitConflict.java
+++ src/main/java/guru/qa/gitConflict.java
@@ -2,8 +2,8 @@ package guru.qa;
 
 public class gitConflict {
     public static void main(String[] args) {
-        String conflictName = "First";
+        String conflictName = "конфликт";
 
-        System.out.println(conflictName);
+        System.out.println("Это " + conflictName);
     }
 }
